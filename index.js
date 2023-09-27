82986f49410ea34fb0c398d864059f1fec2c007d
unknown <mirza.eee.4th@gmail.com>
2023-09-27 17:06:16 +0600

    mytoys and buy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -30,8 +30,37 @@ async function run() {
         await client.connect();
 
         const toysCollection = client.db('toysDB').collection('toys')
+        const toysBuyCollection = client.db('toysDB').collection('toysBuy')
         app.get('/toys', async (req, res) => {
             const result = await toysCollection.find().toArray()
+            res.send(result)
+        })
+        app.post('/toys', async (req, res) => {
+            const toy = req.body;
+            const result = await toysCollection.insertOne(toy)
+            res.send(result)
+
+        })
+
+
+        app.get('/toys/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const filter = { _id: new ObjectId(id) }
+            const result = await toysCollection.findOne(filter)
+
+            res.send(result)
+
+        })
+        app.post('/buy', async (req, res) => {
+            const toy = req.body;
+            console.log(toy);
+            const result = await toysBuyCollection.insertOne(toy)
+            res.send(result)
+
+        })
+        app.get('/mytoys', async (req, res) => {
+            const result = await toysBuyCollection.find().toArray()
             res.send(result)
         })
 
